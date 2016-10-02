4bdb91ef07a3959b9a5a1874a990ab6f857b529f
damianhouse <damianhouse@gmail.com>
2016-10-02 14:01:18 -0400

    Add controller general with welcome view.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161002175024) do
+ActiveRecord::Schema.define(version: 20161002175522) do
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "coach_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "usconversation_id"
+    t.integer  "user_id"
+    t.string   "body"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -25,6 +40,7 @@ ActiveRecord::Schema.define(version: 20161002175024) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
