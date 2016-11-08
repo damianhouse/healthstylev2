5d86983add69cb9ff56e01b86ad984fd2117a7e9
damianhouse <damianhouse@gmail.com>
2016-11-08 11:21:12 -0500

    Change mailer credentials to match those recommended by Heroku.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,9 +2,9 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     super
+    @user
     if @user.persisted?
       UserMailer.welcome(@user).deliver
     end
   end
-
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,16 +61,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              ENV['MAILGUN_SMTP_SERVER'],
-    port:                 ENV['MAILGUN_SMTP_PORT'],
-    domain:               ENV['MAILGUN_SANDBOX_DOMAIN'],
-    user_name:            ENV['MAILGUN_SMTP_LOGIN'],
-    password:             ENV['MAILGUN_SMTP_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
-
+    ActionMailer::Base.smtp_settings = {
+      :port           => ENV['MAILGUN_SMTP_PORT'],
+      :address        => ENV['MAILGUN_SMTP_SERVER'],
+      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain         => ENV['MAILGUN_SANDBOX_DOMAIN'],
+      :authentication => :plain,
+    }
+    ActionMailer::Base.delivery_method = :smtp
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
