5ab9dc8c3189b10266aac301ea3a4ab128724b25
damianhouse <damianhouse@gmail.com>
2016-12-08 11:19:30 -0500

    Update AWS S3 bucket in application file.

--- config/application.rb
+++ config/application.rb
@@ -17,9 +17,9 @@ module Healthstylev2
       :s3_protocol => 'https',
       :path => '/:class/:attachment/:id_partition/:style/:filename',
       :s3_credentials => {
-        :bucket => ENV['AWS_S3_BUCKET'],
-        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+        :bucket => ENV['AWS_S3_BUCKET_V2'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID_V2'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY_V2']
       },
       s3_region: 'us-east-1',
     }
