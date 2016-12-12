613f061dd6941039e67c4d66f3f094046361b6a0
damianhouse <damianhouse@gmail.com>
2016-12-12 10:11:14 -0500

    Refactor subscriptions controller and coupons.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -9,83 +9,43 @@ class SubscriptionsController < ApplicationController
 
   def create
     @user = current_user
-    code = params[:couponCode]
+    @code = normalize_code(params[:couponCode])
     @amount = params[:amount].to_f
     @plan = params[:plan]
     plan_interval = params[:plan_interval]
     interval_count = params[:interval_count].to_i
 
-    if !code.blank?
-      @coupon = Stripe::Coupon.retrieve(normalize_code(code))
-      if @coupon && @coupon.valid
-        @coupon.code = code
-        @discount_percent = @coupon.percent_off
-        @amount_off = @coupon.amount_off
-
-        if @discount_percent
-          @final_amount = (@amount * (1 - @discount_percent))
-          @discount_amount = (@amount - @final_amount)
-          discount_percent_human = @discount_percent.to_s + '%'
-          charge_metadata = {
-            :coupon_code => code,
-            :coupon_discount => discount_percent_human,
-            :plan_interval => plan_interval,
-            :interval_count => interval_count
-          }
-          customer = Stripe::Customer.create(
-            :email => params[:stripeEmail],
-            :source  => params[:stripeToken],
-            :plan => @plan,
-            :coupon => code,
-            :metadata => charge_metadata
-          )
-        elsif @amount_off
-          @final_amount = @amount - @amount_off
-          @discount_amount = (@amount - @final_amount)
-          charge_metadata = {
-            :coupon_code => code,
-            :plan_interval => plan_interval,
-            :interval_count => interval_count
-          }
-          customer = Stripe::Customer.create(
-            :email => params[:stripeEmail],
-            :source  => params[:stripeToken],
-            :plan => @plan,
-            :coupon => code,
-            :metadata => charge_metadata
-          )
-        end
-      else
-        flash[:error] = 'Coupon code is not valid or has expired.'
+    if @code.present?
+      is_valid?(@code, plan_interval, interval_count)
+      if @coupon == nil
         redirect_to subscriptions_new_path
         return
       end
-
     else
-      @final_amount = @amount.to_i
+      @code = nil
       charge_metadata = {
-        :coupon_code => nil,
-        :coupon_discount => nil,
         :plan_interval => plan_interval,
         :interval_count => interval_count
       }
+    end
+
+    begin
       customer = Stripe::Customer.create(
         :email => params[:stripeEmail],
         :source  => params[:stripeToken],
         :plan => @plan,
-        :coupon => nil,
+        :coupon => @code,
         :metadata => charge_metadata
       )
-    end
-
-    @current_user.add_time(plan_interval, interval_count)
-    @current_user.stripe_id = customer.id
-    @current_user.save!
-    flash[:notice] = "Your subscription was successfully created."
-
+      @final_amount = @amount.to_f if @final_amount.nil?
+      @current_user.add_time(plan_interval, interval_count)
+      @current_user.stripe_id = customer.id
+      @current_user.save!
+      flash[:notice] = "Your subscription was successfully created."
     rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to subscriptions_new_path
+      flash[:error] = e.message
+      redirect_to subscriptions_new_path
+    end
   end
 
   def webhook
@@ -113,6 +73,36 @@ class SubscriptionsController < ApplicationController
 
   private
 
+  def is_valid?(coupon, plan_interval, interval_count)
+    begin
+      @coupon = Stripe::Coupon.retrieve(coupon)
+      @discount_percent = @coupon.percent_off
+      @amount_off = @coupon.amount_off
+      if @discount_percent
+        @final_amount = (@amount * (1 - @discount_percent))
+        @discount_amount = (@amount - @final_amount)
+        discount_percent_human = @discount_percent.to_s + '%'
+        charge_metadata = {
+          :coupon_code => coupon,
+          :coupon_discount => discount_percent_human,
+          :plan_interval => plan_interval,
+          :interval_count => interval_count
+        }
+      elsif @amount_off
+        @final_amount = @amount - @amount_off
+        @discount_amount = (@amount - @final_amount)
+        charge_metadata = {
+          :coupon_code => coupon,
+          :coupon_discount => @discount_amount,
+          :plan_interval => plan_interval,
+          :interval_count => interval_count
+        }
+      end
+    rescue Stripe::InvalidRequestError => e
+      flash[:notice] = 'Card not processed because coupon code is not valid or has expired.'
+    end
+  end
+
   def normalize_code(code)
     code.gsub(/ +/, '').upcase
   end
@@ -124,10 +114,6 @@ class SubscriptionsController < ApplicationController
 
   def apply_amount_discount(amount)
     amount - self.discount_amount
-  end
-
-  def handle_success_invoice(user, invoice)
-
   end
 
 end
