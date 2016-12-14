0805de7d438e9c21bd77990b86856f2bf6fcd471
damianhouse <damianhouse@gmail.com>
2016-12-14 08:44:09 -0500

    Add default action mailer host to production file.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.default_url_options = { host: 'www.myhealthstyleapp.com' }
   config.action_mailer.smtp_settings = {
     address:              ENV['MAILGUN_SMTP_SERVER'],
     port:                 ENV['MAILGUN_SMTP_PORT'],
@@ -70,6 +71,7 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true  }
   config.action_mailer.delivery_method = :smtp
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   resources :form_steps
 
   devise_for :users, controllers: { registrations: "registrations" }
+  
   scope '/admin' do
     resources :users
   end
