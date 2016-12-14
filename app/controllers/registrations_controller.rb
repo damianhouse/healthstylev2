c3ab8e66ee157a22065f211ef4453a631e3e6319
damianhouse <damianhouse@gmail.com>
2016-12-14 12:08:49 -0500

    Move notify user to application controller.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -20,21 +20,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
 
-  def notify_user(user)
-    unless user.phone_number == ""
-      begin
-        client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-        message = client.messages.create from: '8284820730', to: user.phone_number,
-
-        body: 'Welcome to MyHealthStyle! We are so excited and will contact you as soon as your coaching team is assigned to you!'
-      rescue
-        flash[:notice] =  "Please enter a valid phone number."
-      end
-    else
-      redirect_to charges_new_path
-    end
-  end
-
   def sign_up(resource_name, resource)
     sign_in(resource_name, resource) unless current_user
   end
