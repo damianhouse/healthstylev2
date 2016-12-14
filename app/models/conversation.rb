b4d412c8ef5f8012c74919a93f5df21f80028b99
damianhouse <damianhouse@gmail.com>
2016-12-14 10:15:26 -0500

    Add validation that a user and coach can only have one conversation.

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,4 +3,5 @@ class Conversation < ApplicationRecord
   belongs_to :coach, class_name: "User"
   has_many :messages, dependent: :destroy
   validates :coach, uniqueness: {scope: :user}
+  validates :user, uniqueness: {scope: :coach}
 end
