88c5018bd489db903b1416f1b06116fc8dc39690
damianhouse <damianhouse@gmail.com>
2016-12-31 16:44:13 -0500

    Remove minimum and maximum message length.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
-  validates :body, presence: true, length: {minimum: 2, maximum: 1000}
+  validates :body, presence: true
 
   def timestamp
     created_at.strftime('%H:%M:%S %d %B %Y')
